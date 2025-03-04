557250ab5ad5608cdfab0917e0711e807d70d5d7
Dennis Priskorn <egils@riseup.net>
2025-03-04 13:20:18 +0100

    feat: Footer: Add link to code

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer mb-3">
       <div className="container">
-        Design and code by
+        Design and <a href="https://github.com/dpriskorn/topic-creator-frontend">code</a> by
         <a href="https://www.wikidata.org/wiki/User:So9q" target="_blank" rel="noopener noreferrer"> Nizo Priskorn</a>. 
         Thanks to Egon Willinghagen and Ainali for suggestions.
         Tip: Try the <a href="https://www.wikidata.org/wiki/User:So9q/wikidata-topic-curator-link.js" target="_blank" rel="noopener noreferrer">user script</a>!
